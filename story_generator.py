9ea53e23cd4cdb98727d214766fbdd95ebed7a91
DVDetroit <115680871+DVDetroit@users.noreply.github.com>
2025-01-16 13:35:27 -0500

    Add files via upload

---
+++ story_generator.py
@@ -0,0 +1,46 @@
+'''
+Goal: Write a short story using Python variables. The story will have 4 sentences and include certain characters, a place, something important, an action, and a goal.
+How to do it:
+
+    Start your Python script by creating six variables. Each variable will hold a piece of information for your story:
+        main_character: The name of your main character.
+        secondary_character: The name of another character.
+        location: Where the story happens.
+        object: Something important in your story.
+        action: What your main character is doing.
+        destination: Where your main character is trying to go.
+    Set (assign) values to these variables. These values will shape your story.
+    Use string concatenation (joining strings together with a +) to create a story from these variables.
+    Your story should have four sentences. Each sentence should include at least one of your variables and make sense in the context of the story.
+    Print your final story so it shows up in the console.
+
+Example: Here's how you might define your variables and print the story:
+main_character = "Alice"
+secondary_character = "the White Rabbit"
+location = "the rabbit hole"
+object = "a mysterious key"
+action = "chasing"
+destination = "a rabbit hole"
+print(main_character + " began " + action + " " + secondary_character + " down " + destination)
+What You Will Achieve: This task helps you understand how to use variables and string concatenation in Python. It's also a fun way to see how coding can be creative!
+Remember:
+
+    Start your script with comments that include these instructions. Comments in Python start with #.  Also, explain what each part of the code is doing in your own words.
+    The provided code is an incomplete example. Create your own completely original, unique story made up of 4 sentences.
+    You must use concatenation (+)
+    Do not use input statements.  Assign values to the variables in the code.
+
+Attach your .py file to Blackboard.
+'''
+
+#declare variables such as people and locations
+main_character = "Tim"
+secondary_character = "King"
+location = "Toledo"
+object = "crown"
+action = "Deliver"
+destination = "castle"
+
+#print the story using variables and dialogue.
+print(main_character + " begins his journey at the behest of the " + secondary_character + ". Seeking the " + object + " in the lands of " + location + ". " + main_character + " will need to return to the " + secondary_character + ". " + main_character + " will " + action + " the " +object + " to the " + secondary_character + " in the " + destination + ".")
+
